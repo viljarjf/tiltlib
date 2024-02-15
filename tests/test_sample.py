68639ae51c72492a5c2ea81fd8ec828555b5b7d6
Viljar Femoen <viljar.femoen@hotmail.no>
2024-02-15 14:50:46 +0100

    Fix concurrent download issue with fixtures

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,5 @@
 import pytest
-from utils import get_xmap, x
+from utils import default_xmap, x
 
 from tiltlib import Axis, Sample
 
@@ -8,9 +8,9 @@ from tiltlib import Axis, Sample
     "initial_angle",
     [0, -10, 10],
 )
-def test_reset_rotation(initial_angle: float):
-    original_xmap = get_xmap()
-    s = Sample(get_xmap(), [Axis(x, -100, 100, initial_angle, degrees=True)])
+def test_reset_rotation(initial_angle: float, x, default_xmap):
+    original_xmap = default_xmap
+    s = Sample(default_xmap, [Axis(x, -100, 100, initial_angle, degrees=True)])
 
     assert s.orientations == original_xmap.orientations.reshape(*original_xmap.shape)
 
@@ -32,9 +32,9 @@ def test_reset_rotation(initial_angle: float):
         [4, 20],
     ],
 )
-def test_initial_angle(angle_1: float, angle_2: float):
-    s1 = Sample(get_xmap(), [Axis(x, -100, 100, angle_1, degrees=True)])
-    s2 = Sample(get_xmap(), [Axis(x, -100, 100, angle_2, degrees=True)])
+def test_initial_angle(angle_1: float, angle_2: float, default_xmap, x):
+    s1 = Sample(default_xmap, [Axis(x, -100, 100, angle_1, degrees=True)])
+    s2 = Sample(default_xmap, [Axis(x, -100, 100, angle_2, degrees=True)])
 
     assert s1.orientations == s2.orientations
 
@@ -58,9 +58,9 @@ def test_initial_angle(angle_1: float, angle_2: float):
     assert s1.orientations == s2.orientations
 
 
-def test_rotate_to():
-    s1 = Sample(get_xmap(), [Axis(x, -100, 100, degrees=True)])
-    s2 = Sample(get_xmap(), [Axis(x, -100, 100, degrees=True)])
+def test_rotate_to(default_xmap, x):
+    s1 = Sample(default_xmap, [Axis(x, -100, 100, degrees=True)])
+    s2 = Sample(default_xmap, [Axis(x, -100, 100, degrees=True)])
 
     assert s1.orientations == s2.orientations
 
@@ -85,9 +85,9 @@ def test_rotate_to():
     assert s1.orientations == s2.orientations
 
 
-def test_rotate_to_with_initial_angle():
-    s1 = Sample(get_xmap(), [Axis(x, -100, 100, 10, degrees=True)])
-    s2 = Sample(get_xmap(), [Axis(x, -100, 100, degrees=True)])
+def test_rotate_to_with_initial_angle(x, default_xmap):
+    s1 = Sample(default_xmap, [Axis(x, -100, 100, 10, degrees=True)])
+    s2 = Sample(default_xmap, [Axis(x, -100, 100, degrees=True)])
 
     assert s1.orientations == s2.orientations
 
@@ -116,8 +116,8 @@ def test_rotate_to_with_initial_angle():
     assert s1.orientations == s2.orientations
 
 
-def test_rotating_to_initial_angle():
-    s1 = Sample(get_xmap(), [Axis(x, -100, 100, degrees=True)])
+def test_rotating_to_initial_angle(x, default_xmap):
+    s1 = Sample(default_xmap, [Axis(x, -100, 100, degrees=True)])
 
     s1.rotate_to(5, degrees=True)
 
--- tests/utils.py
+++ tests/utils.py
@@ -1,17 +1,32 @@
 from numpy import allclose
 from orix.vector import Vector3d
+from orix.data import sdss_austenite
 
+from pytest import fixture
 
 def vec_eq(v1: Vector3d, v2: Vector3d) -> bool:
     return allclose(v1.data, v2.data)
 
+@fixture
+def x() -> Vector3d:
+    return Vector3d.xvector()
+@fixture
+def y() -> Vector3d:
+    return Vector3d.yvector()
+@fixture
+def z() -> Vector3d:
+    return Vector3d.zvector()
 
-x = Vector3d.xvector()
-y = Vector3d.yvector()
-z = Vector3d.zvector()
+# Adapt https://github.com/pytest-dev/pytest/issues/1402#issuecomment-186299177
+# to avoid pytest issues with attempting to download the dataset multiple times
 
-from orix.data import sdss_austenite
+def pytest_configure(config):        
+    if is_master(config):
+        sdss_austenite(allow_download=True)
 
+def is_master(config):
+    return hasattr(config, "workerinput")
 
-def get_xmap():
-    return sdss_austenite(allow_download=True)
+@fixture
+def default_xmap():
+    return sdss_austenite(allow_download=False)
