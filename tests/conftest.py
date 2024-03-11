e810a10a0ce76cb47f37825c00027c0d25494444
Viljar Femoen <viljar.femoen@hotmail.no>
2024-03-11 13:13:01 +0100

    Properly fix downloading orix dataset (again)

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,36 +25,17 @@ def z() -> Vector3d:
     return Vector3d.zvector()
 
 
-# Adapt https://github.com/pytest-dev/pytest/issues/1402#issuecomment-186299177
+# Adapt https://stackoverflow.com/questions/17801300/how-to-run-a-method-before-all-tests-in-all-classes
 # to avoid pytest issues with attempting to download the dataset multiple times
 
 
-def pytest_configure(config):
-    print("Start")
-    if is_master(config):
-        sdss_austenite(allow_download=True)
-
-
-def is_master(config):
-    return hasattr(config, "workerinput")
+def pytest_sessionstart(session):
+    sdss_austenite(allow_download=True)
 
 
 # Kinda hacky, but this ensures only a single download for the whole (parallel) session.
 # Taken from the xdist documentation:
 # https://pytest-xdist.readthedocs.io/en/latest/how-to.html#making-session-scoped-fixtures-execute-only-once
 @pytest.fixture(scope="session")
-def default_xmap(tmp_path_factory, worker_id):
-    if worker_id == "master":
-        return sdss_austenite(allow_download=True)
-
-    root_tmp_dir = tmp_path_factory.getbasetemp().parent
-
-    fn = root_tmp_dir / "default_xmap.tmp"
-
-    with FileLock(str(fn) + ".lock"):
-        if fn.is_file():
-            data = sdss_austenite(allow_download=False)
-        else:
-            data = sdss_austenite(allow_download=True)
-            fn.write_text("Download complete")
-    return data
+def default_xmap():
+    return sdss_austenite(allow_download=False)
